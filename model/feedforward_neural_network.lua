84cef16301fa03557cd18e70f43264c6c6abec77
pandevirus@gmail.com <pandevirus@gmail.com>
2015-10-04 15:35:02 -0700

    Update fnn

--- model/feedforward_neural_network.lua
+++ model/feedforward_neural_network.lua
@@ -1,3 +1,4 @@
+-- Dataset
 dataset = {};
 function dataset:size() return 100 end
 for i = 1, dataset:size() do
@@ -9,5 +10,24 @@ for i = 1, dataset:size() do
         output[1] = 1;
     end
     dataset[i] = {input, output}
-    print(dataset)
 end
+
+-- Neural Network
+require "nn"
+mlp = nn.Sequential();
+inputs = 2; outputs = 1; HUs = 20;
+mlp:add(nn.Linear(inputs, HUs))
+mlp:add(nn.Tanh())
+mlp:add(nn.Linear(HUs, outputs))
+
+-- Training
+criterion = nn.MSECriterion()
+trainer = nn.StochasticGradient(mlp, criterion)
+trainer.learningRate = 0.01
+trainer:train(dataset)
+
+x = torch.Tensor(2)
+x[1] = 0.5; x[2] = 0.5; print(mlp:forward(x))
+x[1] = 0.5; x[2] = -0.5; print(mlp:forward(x))
+x[1] = -0.5; x[2] = 0.5; print(mlp:forward(x))
+x[1] = -0.5; x[2] = -0.5; print(mlp:forward(x))
